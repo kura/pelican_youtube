57a39865d6d7e09f25c6940432d7cbc531df4574
Peter Bittner <django@bittner.it>
2025-03-11 16:34:07 +0100

    Use f-strings instead of format method

--- pelican/plugins/youtube/youtube.py
+++ pelican/plugins/youtube/youtube.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # Copyright (c) 2013 Kura
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,14 +17,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from __future__ import unicode_literals
-
 from docutils import nodes
 from docutils.parsers.rst import directives, Directive
 
 
 class YouTube(Directive):
-    """ Embed YouTube video in posts.
+    """Embed YouTube video in posts.
 
     Based on the YouTube directive by Brian Hsu:
     https://gist.github.com/1422773
@@ -63,7 +59,7 @@ class YouTube(Directive):
         youtube_domain = (
             "youtube-nocookie" if "nocookie" in self.options else "youtube"
         )
-        url = "https://www.{}.com/embed/{}".format(youtube_domain, videoID)
+        url = f"https://www.{youtube_domain}.com/embed/{videoID}"
 
         width = self.options.get("width", None)
         height = self.options.get("height", None)
@@ -71,10 +67,10 @@ class YouTube(Directive):
         seamless = self.options.get("seamless", True)
         css_classes = "youtube"
         if "class" in self.options:
-            css_classes += " {}".format(self.options["class"])
+            css_classes += f" {self.options['class']}"
         elif height is None:
             # use responsive design with 16:9 aspect ratio by default
-            css_classes += " {}".format("youtube-16x9")
+            css_classes += " youtube-16x9"
         # no additional classes if dimensions (height/width) are specified
 
         iframe_arguments = [
@@ -84,8 +80,8 @@ class YouTube(Directive):
             (seamless, 'seamless frameBorder="0"'),
         ]
 
-        div_block = '<div class="{}">'.format(css_classes)
-        embed_block = '<iframe src="{}" '.format(url)
+        div_block = f'<div class="{css_classes}">'
+        embed_block = f'<iframe src="{url}" '
 
         for value, format in iframe_arguments:
             embed_block += (format + " ").format(value) if value else ""
